f82933c798295c771978bca52df3baad0f3f6ca7
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2019-10-04 13:00:07 +0200

    release-04-00-00 added to supported releases, release 2 versions removed

--- versioning.py
+++ versioning.py
@@ -9,8 +9,8 @@ from distutils.version import LooseVersion
 
 # list of supported releases, the last one is the recommended one
 _supported_releases = [
-    'release-02-00-02', 'release-02-01-00',
-    'release-03-01-04', 'release-03-02-04']
+    'release-03-01-04', 'release-03-02-04',
+    'release-04-00-00']
 
 # list of supported light releases
 _supported_light_releases = [
