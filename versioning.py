e12d7311790a87577c63f65a82699cf91993b4e0
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2019-11-22 17:29:17 +0100

    release-04-00-03 --> release-04-00-04

--- versioning.py
+++ versioning.py
@@ -14,7 +14,7 @@ import shutil
 # list of supported releases, the last one is the recommended one
 _supported_releases = [
     'release-03-01-04', 'release-03-02-04',
-    'release-04-00-03']
+    'release-04-00-04']
 
 # list of supported light releases
 _supported_light_releases = [
