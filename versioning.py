44a8caa46b4729da967ac703a01a023d77605ad5
Frank Meier <frank.meier@duke.edu>
2022-04-02 07:58:07 +0900

    update recommended light release to zeus

--- versioning.py
+++ versioning.py
@@ -11,7 +11,7 @@ import os
 import shutil
 
 # recommended release
-_recommended_release = 'light-2112-uranus'
+_recommended_release = 'light-2203-zeus'
 
 # list of supported full releases
 _supported_releases = [
@@ -21,7 +21,7 @@ _supported_releases = [
 
 # list of supported light releases
 _supported_light_releases = [
-    'light-2104-poseidon', 'light-2110-tartarus', 'light-2112-uranus'
+    'light-2104-poseidon', 'light-2110-tartarus', 'light-2201-venus', 'light-2203-zeus'
 ]
 
 assert _supported_releases == sorted(_supported_releases)
