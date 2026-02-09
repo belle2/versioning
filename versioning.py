297b7837beac05a1543f469b52eec78929533fd9
Vidya Sagar VOBBILISETTI <vidya.vobbilisetti@pi.infn.it>
2026-02-09 14:15:48 +0100

    Replace distutils with standalone looseversion

--- versioning.py
+++ versioning.py
@@ -4,7 +4,6 @@
 Management of software versions and global tags.
 """
 
-from distutils.version import LooseVersion
 import json
 import os
 import re
@@ -35,6 +34,91 @@ assert _supported_releases == sorted(_supported_releases)
 assert _supported_pre_releases == sorted(_supported_pre_releases)
 assert _supported_light_releases == sorted(_supported_light_releases)
 
+class LooseVersion(object):
+    """
+    A standalone implementation of version number comparisons.
+    
+    Numeric components are compared numerically, while alphabetic components 
+    are compared lexically.
+
+    Replaces the deprecated `distutils.version.LooseVersion` using the 
+    backwards-compatible fork at https://github.com/effigies/looseversion.
+    Licensed under the PSF License Version 2.
+    """
+
+    component_re = re.compile(r"(\d+ | [a-z]+ | \.)", re.VERBOSE)
+
+    def __init__(self, vstring=None):
+        if vstring:
+            self.parse(vstring)
+
+    def __eq__(self, other):
+        c = self._cmp(other)
+        if c is NotImplemented:
+            return NotImplemented
+        return c == 0
+
+    def __lt__(self, other):
+        c = self._cmp(other)
+        if c is NotImplemented:
+            return NotImplemented
+        return c < 0
+
+    def __le__(self, other):
+        c = self._cmp(other)
+        if c is NotImplemented:
+            return NotImplemented
+        return c <= 0
+
+    def __gt__(self, other):
+        c = self._cmp(other)
+        if c is NotImplemented:
+            return NotImplemented
+        return c > 0
+
+    def __ge__(self, other):
+        c = self._cmp(other)
+        if c is NotImplemented:
+            return NotImplemented
+        return c >= 0
+
+    def parse(self, vstring):
+        self.vstring = vstring
+        components = [x for x in self.component_re.split(vstring) if x and x != "."]
+        for i, obj in enumerate(components):
+            try:
+                components[i] = int(obj)
+            except ValueError:
+                pass
+
+        self.version = components
+
+    def __str__(self):
+        return self.vstring
+
+    def __repr__(self):
+        return "LooseVersion ('%s')" % str(self)
+
+    def _cmp(self, other):
+        other = self._coerce(other)
+        if other is NotImplemented:
+            return NotImplemented
+
+        if self.version == other.version:
+            return 0
+        if self.version < other.version:
+            return -1
+        if self.version > other.version:
+            return 1
+        return NotImplemented
+
+    @classmethod
+    def _coerce(cls, other):
+        if isinstance(other, cls):
+            return other
+        elif isinstance(other, str):
+            return cls(other)
+        return NotImplemented
 
 def supported_release(release=None):
     """
